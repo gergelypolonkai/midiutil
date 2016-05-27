fc5cd7fdbda9682657746d420e7298825130d558
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-05-27 16:35:12 +0200

    Update to setuptools usage

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
-setup(name='MIDIUtil',
-      version='0.87',
-      description='MIDIUtil, a MIDI Interface for Python',
-      author='Mark Conway Wirt',
-      author_email='emergentmusics) at (gmail . com',
-      license='Copyright (C) 2009, Mark Conway Wirt. See License.txt for details.',
-      url='http://www.emergentmusics.org/midiutil/',
-      packages=["midiutil"],
-      package_dir = {'midiutil': 'src/midiutil'},
-      package_data={'midiutil' : ['../../documentation/*']},
-      scripts=['examples/single-note-example.py'],
-      platforms='Platform Independent',
-      long_description='''
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='MIDIUtil',
+    version='0.87',
+    description='MIDIUtil, a MIDI Interface for Python',
+    author='Mark Conway Wirt',
+    author_email='emergentmusics) at (gmail . com',
+    url='http://www.emergentmusics.org/midiutil/',
+    license='Copyright (C) 2009, Mark Conway Wirt. See License.txt for details.',
+    packages=['midiutil'],
+    package_dir = {'midiutil': 'src/midiutil'},
+    package_data={'midiutil' : ['../../documentation/*']},
+    scripts=['examples/single-note-example.py'],
+    long_description='''
 This package provides a simple interface to allow Python programs to
-write multi-track MIDI files.'''
-     )
+write multi-track MIDI files.''',
+    classifiers=[
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+        ]
+)
